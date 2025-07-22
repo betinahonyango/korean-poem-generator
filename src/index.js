8e65e134e374a52b26877c56098c65c599d367cc
Betinah <betinahonyango@gmail.com>
2025-07-22 09:17:25 +0300

    Added AI API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem (response){
   new Typewriter ("#my-poem" , {
-    strings: "Oh, my lover who had pure eyes <br/> 말간 눈을 한 애인이여",
+    strings: response.data.answer,
     autoStart: true,
     delay: 10,
     cursor: "" ,
-  })
+  })  
+}
+function generatePoem(event) {
+  event.preventDefault();
+ 
+ let instructionsInput = document.getElementById("user-instructions")
+ let apikey = "50do3122d9528t350cd40fbf4dc0035a";
+ let prompt = `User instructions: Generate a Korean poem about ${instructionsInput.value}`
+ let context = `You are a romantic poem expert and you love to write short poems. You are in charge generating 4 line poems in korean then translated to english. Make sure to follow user instructions and write in basic html without displaying that it is in html.`
+ let apiurl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apikey}`;
+
+ axios.get(apiurl).then(displayPoem);
+  
 }
 let poemFormElement = document.getElementById("form-generator")
 poemFormElement.addEventListener("submit", generatePoem)
